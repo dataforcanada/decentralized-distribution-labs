88183e14436068884488ff2bd922793c9a7e01f1
Diego Ripley <diego@diegoripley.ca>
2026-02-07 10:55:06 -0500

    Add Source Coop as an HTTP webseed

--- scripts/01_automate_creation_torrent_basic_tuning/run.sh
+++ scripts/01_automate_creation_torrent_basic_tuning/run.sh
@@ -2,16 +2,17 @@ DATA_DIR="${HOME}/Projects/dataforcanada/decentralized-distribution-labs/data"
 DATASET_ID="ca-ab_edmonton-2023A00054811061_orthoimagery_2023_075mm"
 CORES=$(nproc)
 
-# Piece length (8 MiB) based on https://academictorrents.com/docs/uploading.html
+# Piece length (16 MiB) based on https://academictorrents.com/docs/uploading.html
 echo "Creating ${DATASET_ID}.pmtiles.torrent"
 py3createtorrent \
     --threads ${CORES} \
     --private \
-    --piece-length 8192 \
+    --piece-length 16384 \
     --md5 \
     --source "Data for Canada" \
     --comment "City of Edmonton Orthoimagery for 2023 / Ortho-imagerie de la Ville de Edmonton de 2023" \
     --tracker "https://tracker.labs.dataforcanada.org/announce" \
+    --webseed "https://data.source.coop/dataforcanada/ca-orthoimagery-labs/ca-ab_edmonton-2023A00054811061_orthoimagery_2023_075mm.pmtiles" \
     --webseed "https://data-01.labs.dataforcanada.org/processed/ca-ab_edmonton-2023A00054811061_orthoimagery_2023_075mm.pmtiles" \
     --webseed "https://data-02.labs.dataforcanada.org/processed/ca-ab_edmonton-2023A00054811061_orthoimagery_2023_075mm.pmtiles" \
     "${DATA_DIR}/${DATASET_ID}.pmtiles" \
